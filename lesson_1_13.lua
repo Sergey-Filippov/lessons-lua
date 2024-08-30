3c9b876e4d945a700c27c0870435e13663aa29d5
Sergey-Filippov <investfx09@gmail.com>
2024-08-30 09:36:33 +0300

    Add file lesson_1_13

---
+++ lesson_1_13.lua
@@ -0,0 +1,40 @@
+-- Найти периметр и площадь равнобедренной трапеции 
+-- с  основаниями  a  и  b  (a  >  b)  и  углом  alpha  при 
+-- большем основании (угол дан в радианах).
+function arifmetic(a,b,alfa)
+  local h = ((a-b)/2)*math.tan(alfa)  
+  local c = h/math.sin(alfa)
+  P = math.floor((a+b+2*c)*100 +0.5)/100
+  S = math.floor( (h*(a+b)/2)*100 +0.5)/100
+  return P,S
+end
+function read_data()
+    repeat
+        if a == nil or 0 then
+            print("Введите длину больщего основания 'а',см")  
+            a  =tonumber(io.read())
+        end
+        if type(a) ~= "number" then a =0 end
+        if b == nil or 0 then
+            print("Введите длину меньшего основания 'b',см")  
+            b  =tonumber(io.read())
+        end
+        if type(b) ~= "number" then b =0 end   
+        if a <= b then 
+            print("Основание а должно быть больше основания b") 
+            a,b = 0,0
+        end
+        if alfa == nil or 0 then
+            print("Введите угол при основании 'a' в радианах")  
+            alfa  =tonumber(io.read())
+        end
+        if type(alfa) ~= "number" then alfa =0 end
+    until a>0 and b>0 and alfa>0 and alfa<math.pi/2
+	return a,b,alfa
+end
+------
+do
+P, S = arifmetic(read_data())
+print("Периметр равнобедренной трапеции = "..P.."см".."\n"..
+"Площадь трапеции = "..S.."кв.см.")
+end

cad259d15de110203923584fad54ea076266b6a1
Sergey-Filippov <investfx09@gmail.com>
2024-09-06 09:06:16 +0300

    prototype

---
+++ lesson_1_18.lua
@@ -0,0 +1,17 @@
+-- Найти решение системы уравнений вида A1x + B1y = 
+-- C1,  A2x  +  B2y  =  C2,  заданной  своими 
+-- коэффициентами  A1,  B1,  C1,  A2,  B2,  C2
+----------
+function equation(a1,b1,c1,a2,b2,c2)
+
+    return x,y
+end
+-------------
+function read_data()
+
+    return a1,b1,c1,a2,b2,c2
+end
+----------
+do
+    
+end
